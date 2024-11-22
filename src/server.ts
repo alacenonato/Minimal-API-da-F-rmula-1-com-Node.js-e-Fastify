7e5d95db08a4f437a0eb94a23fc1325d93eb6b10
Alace Pereira Nonato <alac_pereira@hotmail.com>
2024-11-22 17:54:37 -0300

    Crete a fast API

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,15 @@
-console.log("hello world");
+import fastify from "fastify";
+
+const server = fastify({logger: true});
+
+server.get('/teams', async (request, response) => {
+    response.type("application/json").code(200);
+
+    return [
+        {id: 1, name: "ferrari"}
+    ];
+});
+
+server.listen({port: 3000}, () => {
+    console.log("Server init");    
+})
