535c7982c8331df8fb7fdc7d173fa6f55dd62352
Alace Pereira Nonato <alac_pereira@hotmail.com>
2024-11-22 18:45:08 -0300

    Cors: Erro de Navegador

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
 import fastify from "fastify";
+import cors from "@fastify/cors";
+
 
 const server = fastify({logger: true});
 
-
+server.register(cors, {
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+});
 
 const teams = [
     {id: 1, name: "McLaren", base: "Working, United Kingdom"},
